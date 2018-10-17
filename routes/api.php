88e081f687c298c435807a845c78e7406c050f3a
Nemanja Gajic <nemanja.gajic@pbssolutions.net>
2018-10-17 13:53:04 +0200

    authentication added

--- routes/api.php
+++ routes/api.php
@@ -11,16 +11,33 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::group([
-    'middleware' => 'cors'
+    'middleware' => 'cors',
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('register', 'AuthController@register');
+
+    Route::group([
+        'middleware' => 'auth:api'
+    ], function () {
+        Route::get('logout', 'AuthController@logout');
+        Route::get('user', 'AuthController@user');
+    });
+});
+
+Route::group([
+    'middleware' => 'cors',
+    'middleware' => 'auth:api'
 ], function ($router) {
     Route::apiResource('/beers', 'BeerController');
 });
 
 Route::group([
-    'middleware' => 'cors'
+    'middleware' => 'cors',
+    'middleware' => 'auth:api'
 ], function ($router) {
     Route::apiResource('/customers', 'CustomerController');
     Route::get('/orders', 'OrderController@index');
